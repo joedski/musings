80b475238ead8add8ce6e06713613e7463de0d50
Joe Sikorski <joe.sikorski@ge.com>
2018-03-06 13:00:42 -0500

    Router working, basic chart put in.

--- Highcharts/highcharts-in-vue/src/views/BasicChart.vue
+++ Highcharts/highcharts-in-vue/src/views/BasicChart.vue
@@ -1,11 +1,42 @@
 <template lang="html">
-  <div>Basic chart...</div>
+  <div class="container">
+    <h1>Basic Chart</h1>
+    <div class="chart-container">
+      <resize-observer @notify="$refs.chart.chart.reflow()" />
+      <highcharts ref="chart" :options="chartOptions" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      chartOptions: {
+        title: { text: 'Fruit Consumption' },
+        chart: {
+          type: 'column',
+          reflow: false,
+        },
+        series: [
+          {
+            name: 'Jane',
+            data: [5, 2, 9],
+          },
+          {
+            name: 'John',
+            data: [3, 4, 1],
+          },
+        ],
+      },
+    }
+  },
 }
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
+.chart-container {
+  max-width: 800px;
+  margin: auto;
+}
 </style>

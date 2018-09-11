44b194cf4445d6a90cf52ffaa1b50e95e38cf987
Joe Sikorski <joe.sikorski@ge.com>
2018-09-11 17:11:54 -0400

    TodoMVC hyperhtml test: Basic most streaming

--- Javascript/TodoMVC Tests/hyperhtml/src/index.js
+++ Javascript/TodoMVC Tests/hyperhtml/src/index.js
@@ -1,6 +1,30 @@
 import 'todomvc-app-css/index.css';
 import hyper from 'hyperhtml';
+import { now, constant, tap, runEffects } from '@most/core';
+import { newDefaultScheduler } from '@most/scheduler';
 
 const appEl = document.getElementById('app');
 
-hyper(appEl)`<section className="todoapp">hi</section>`;
+/**
+ * This function actually flushes changes to the DOM,
+ * by calling `hyperHTML.bind` on the root element.
+ *
+ * hyperHTML itself keeps things performant by caching under the hood
+ * based on both the template string-parts inputs and
+ * the actual argument/interpolation inputs.
+ */
+function redraw(state) {
+  hyper.bind(appEl)`<section class="todoapp">
+    <header class="header">
+      <h1>todos</h1>
+      <input type="text" class="new-todo" placeholder="What needs to be done?" />
+    </header>
+  </section>`;
+}
+
+const render = tap(
+  redraw,
+  constant({}, now()),
+);
+
+runEffects(render, newDefaultScheduler());

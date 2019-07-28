6dfc1e8c9b3dff7adf311663d16575e7790d4713
Joseph Sikorski <joedski@gmail.com>
2019-07-28 18:12:03 -0400

    Improve output of iterate-routes.listAppRoutes()

--- Javascript/Journal 2019-07-21 - On Swaggerdocifying Koa and Koa Router (Files)/iterate-routes.js
+++ Javascript/Journal 2019-07-21 - On Swaggerdocifying Koa and Koa Router (Files)/iterate-routes.js
@@ -1,24 +1,39 @@
 const Router = require('koa-router');
 
+function indent(level, string) {
+  const indentString = '  '.repeat(level);
+  return string.split('\n').map(s => indentString + s).join('\n');
+}
+
 function listAppRoutes(koaApp) {
   koaApp.middleware.forEach(middleware => {
     const { router } = middleware;
     if (router && router instanceof Router) {
-      console.log(`Router: prefix=${router.opts.prefix || ''}`);
-      listRouterRoutes(router);
+      listRouterRoutes(router, 0);
     }
   });
 }
 
 function listRouterRoutes(koaRouter, level = 1) {
-  const indent = '  '.repeat(level);
+  console.log(indent(level, `Router: prefix=${koaRouter.opts.prefix || ''}`));
+
   koaRouter.stack.forEach(layer => {
-    console.log(`${indent}Layer: path="${layer.path}" methods=[${layer.methods.join(',')}] paramNames=${JSON.stringify(layer.paramNames)} opts=${JSON.stringify(layer.opts || {})}`);
+    console.log(indent(level + 1, `Layer:
+  path="${layer.path}"
+  methods=[${layer.methods.join(',')}]
+  paramNames=${
+    layer.paramNames.length
+    ? '\n' + indent(level + 2, layer.paramNames.map(it => JSON.stringify(it)).join('\n'))
+    : ''
+}
+  opts=${JSON.stringify(layer.opts || {})}
+  middleware=`));
     layer.stack.forEach(middleware => {
       const { router } = middleware;
       if (router && router instanceof Router) {
-        console.log(`${indent}  Router: prefix=${router.opts.prefix || ''}`);
-        listRouterRoutes(router, level + 2);
+        listRouterRoutes(router, level + 3);
+      } else {
+        console.log(indent(level + 3, '(fn)'));
       }
     });
   });

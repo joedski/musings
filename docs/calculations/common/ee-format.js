9354c36031a838252260a93ed8fb0290135a95b3
Joseph Sikorski <joedski@gmail.com>
2020-11-07 20:27:25 -0500

    Added nearestSeriesValueFromValue so I can get them E-series values

--- docs/calculations/common/ee-format.js
+++ docs/calculations/common/ee-format.js
@@ -66,7 +66,7 @@ function exponentFromUnitText(unitText) {
     return PREFIX_EXPONENT[unitText];
   }
 
-  const prefix = unitText.substr(0, 1);
+  const prefix = unitText.substring(0, 1);
 
   if (prefix in PREFIX_EXPONENT) {
     return PREFIX_EXPONENT[prefix];
@@ -74,4 +74,65 @@ function exponentFromUnitText(unitText) {
 
   // Assuming it's a unit instead of a prefix.
   return 0;
+}
+
+export function nearestSeriesValueFromValue(value, series) {
+  const exponent = exponentFromValue(value);
+  // Now on range of 1.00... to 9.99...
+  // ... probably.
+  const significand = value / (10 ** exponent);
+
+  const nextLower = nextLowerSeriesValueFromSignificand(significand, series);
+  const nextHigher = nextHigherSeriesValueFromSignificand(significand, series);
+
+  const closest = rationallyCloserValue(significand, nextLower, nextHigher);
+
+  return closest * 10 ** exponent;
+}
+
+export function nearestE12ValueFromValue(value) {
+  return nearestSeriesValueFromValue(value, ESERIES.E12);
+}
+
+export function exponentFromValue(value) {
+  // Honestly this feels kinda hax, and will probably invite strange edge cases.
+  const exponentialText = value.toExponential();
+  const exponentIndicatorIndex = exponentialText.indexOf('e');
+  const exponentText = exponentialText.substring(exponentIndicatorIndex + 1);
+  return parseInt(exponentText, 10);
+}
+
+export function nextLowerSeriesValueFromSignificand(significand, series) {
+  // We don't have a findRight, so we get this instead.
+  return series.reduce(
+    (current, seriesValue) => {
+      if (seriesValue <= significand) {
+        return seriesValue;
+      }
+
+      return current;
+    },
+    0
+  );
+}
+
+export function nextHigherSeriesValueFromSignificand(significand, series) {
+  const nextHighestMaybe = series.find(seriesValue => seriesValue >= significand);
+  return nextHighestMaybe == null ? 10 : nextHighestMaybe;
+}
+
+export function rationallyCloserValue(value, lower, higher) {
+  // if value is 1.05, closer is 1.0.
+  //   1.0/1.05 = 0.9524; 1.05/1.2 = 0.875
+  // if value is 1.15, closer is 1.2.
+  //   1.0/1.15 = 0.8696; 1.15/1.2 = 0.9583
+
+  const lowerRatio = lower / value;
+  const higherRatio = value / higher;
+
+  if (higherRatio > lowerRatio) {
+    return higher;
+  }
+
+  return lower;
 }
--- docs/calculations/common/ee-format.test.js
+++ docs/calculations/common/ee-format.test.js
@@ -1,6 +1,12 @@
 import {
   FLOAT_TEXT_WITH_UNIT_RE,
+  ESERIES,
   floatFromExponentialWithUnit,
+  nearestSeriesValueFromValue,
+  exponentFromValue,
+  nextLowerSeriesValueFromSignificand,
+  nextHigherSeriesValueFromSignificand,
+  rationallyCloserValue,
 } from './ee-format.js';
 
 describe('FLOAT_TEXT_WITH_UNIT_RE', () => {
@@ -71,56 +77,47 @@ describe('FLOAT_TEXT_WITH_UNIT_RE', () => {
 });
 
 describe('floatFromExponentialWithUnit', () => {
+  const withinDigits = 8;
   const testCaseList = [
     {
       text: '1',
       expected: 1,
-      withinDigits: 8
     },
     {
       text: '1.4',
       expected: 1.4,
-      withinDigits: 8
     },
     {
       text: '1.4e2',
       expected: 1.4e2,
-      withinDigits: 8
     },
     {
       text: '1.4e-2',
       expected: 1.4e-2,
-      withinDigits: 8
     },
     {
       text: '1.4e-2',
       expected: 1.4e-2,
-      withinDigits: 8
     },
     {
       text: '1.4Ω',
       expected: 1.4,
-      withinDigits: 8
     },
     {
       text: '1.4mV',
       expected: 1.4e-3,
-      withinDigits: 8
     },
     {
       text: '1.4k',
       expected: 1.4e3,
-      withinDigits: 8
     },
     {
       text: '1.4µ',
       expected: 1.4e-6,
-      withinDigits: 8
     },
     {
       text: '7.56M',
       expected: 7.56e6,
-      withinDigits: 8
     },
   ];
 
@@ -128,7 +125,181 @@ describe('floatFromExponentialWithUnit', () => {
     test(`should parse "${testCase.text}" to ${testCase.expected} (within ${testCase.withinDigits} digits)`, () => {
       const result = floatFromExponentialWithUnit(testCase.text);
       expect(result)
-        .toBeCloseTo(testCase.expected, testCase.withinDigits);
+        .toBeCloseTo(testCase.expected, withinDigits);
+    });
+  }
+});
+
+describe('nearestSeriesValueFromValue', () => {
+  describe('E12 series values', () => {
+    const withinDigits = 4;
+    const series = ESERIES.E12;
+    const testCaseList = [
+      {
+        value: 12,
+        expectedNearest: 12,
+      },
+      {
+        value: 10,
+        expectedNearest: 10,
+      },
+      {
+        value: 12.6,
+        expectedNearest: 12,
+      },
+      {
+        value: 8000,
+        expectedNearest: 8200,
+      },
+      {
+        value: 8350,
+        expectedNearest: 8200,
+      },
+      {
+        value: 0.097,
+        expectedNearest: 0.1,
+      },
+      {
+        value: 85e-6,
+        expectedNearest: 82e-6,
+      },
+      {
+        value: 3.4e3,
+        expectedNearest: 3.3e3,
+      },
+    ];
+
+    for (const testCase of testCaseList) {
+      test(`given value ${testCase.value}, should yield ${testCase.expectedNearest}`, () => {
+        const result = nearestSeriesValueFromValue(testCase.value, series);
+        expect(result).toBeCloseTo(testCase.expectedNearest, withinDigits);
+      });
+    }
+  });
+});
+
+describe('exponentFromValue', () => {
+  const testCaseList = [
+    {
+      value: 1,
+      expectedExponent: 0,
+    },
+    {
+      value: 1200,
+      expectedExponent: 3,
+    },
+    {
+      value: 0.05,
+      expectedExponent: -2,
+    },
+    {
+      value: 65e3,
+      expectedExponent: 4,
+    },
+  ];
+
+  for (const testCase of testCaseList) {
+    test(`given value ${testCase.value}, should yield exponent ${testCase.expectedExponent}`, () => {
+      const result = exponentFromValue(testCase.value);
+      expect(result).toEqual(testCase.expectedExponent);
+    });
+  }
+});
+
+describe('nextLowerSeriesValueFromSignificand', () => {
+  describe('E12 series values', () => {
+    const series = ESERIES.E12;
+    const testCaseList = [
+      // NOTE: should work because they're literals...
+      {
+        value: 1.2,
+        expected: 1.2,
+      },
+      {
+        value: 1.1,
+        expected: 1.0,
+      },
+      {
+        value: 3.4,
+        expected: 3.3,
+      },
+      {
+        value: 8.3,
+        expected: 8.2,
+      },
+      {
+        value: 9.5,
+        expected: 8.2,
+      },
+    ];
+
+    for (const testCase of testCaseList) {
+      test(`given value ${testCase.value}, should yield ${testCase.expected}`, () => {
+        const result = nextLowerSeriesValueFromSignificand(testCase.value, series);
+        expect(result).toEqual(testCase.expected);
+      });
+    }
+  });
+});
+
+describe('nextHigherSeriesValueFromSignificand', () => {
+  describe('E12 series values', () => {
+    const series = ESERIES.E12;
+    const testCaseList = [
+      // NOTE: should work because they're literals...
+      {
+        value: 1.2,
+        expected: 1.2,
+      },
+      {
+        value: 1.1,
+        expected: 1.2,
+      },
+      {
+        value: 3.4,
+        expected: 3.9,
+      },
+      {
+        value: 8.3,
+        expected: 10,
+      },
+    ];
+
+    for (const testCase of testCaseList) {
+      test(`given value ${testCase.value}, should yield ${testCase.expected}`, () => {
+        const result = nextHigherSeriesValueFromSignificand(testCase.value, series);
+        expect(result).toEqual(testCase.expected);
+      });
+    }
+  });
+});
+
+describe('rationallyCloserValue', () => {
+  const testCaseList = [
+    {
+      value: 1.05,
+      lower: 1,
+      higher: 1.2,
+      expected: 1,
+    },
+    {
+      value: 1.15,
+      lower: 1,
+      higher: 1.2,
+      expected: 1.2,
+    },
+    {
+      value: 3.4,
+      lower: 3.3,
+      higher: 3.9,
+      expected: 3.3,
+    },
+  ];
+
+  for (const testCase of testCaseList) {
+    test(`given value ${testCase.value} and lower and higher numbers (${testCase.lower}, ${testCase.higher}), should yield ${testCase.expected}`, () => {
+      const result = rationallyCloserValue(testCase.value, testCase.lower, testCase.higher);
+      expect(result).toEqual(testCase.expected);
     });
   }
 });

52024b0dd1520b9c0a168e207a5538fa2bc4bbfb
Joseph Sikorski <joedski@gmail.com>
2020-11-07 21:17:22 -0500

    Add Nearest E12 value for R2 in LM317 calculator

--- docs/calculations/regulator-linear-lm317-various/resistors-from-vout.js
+++ docs/calculations/regulator-linear-lm317-various/resistors-from-vout.js
@@ -1,5 +1,9 @@
 // resistors-from-vout.js
 
+import {
+  nearestE12ValueFromValue,
+} from '../common/ee-format.js';
+
 function addEventListenerToId(elementId, eventName, handler, ...otherOptions) {
   const el = document.getElementById(elementId);
   el.addEventListener(eventName, handler, ...otherOptions);
@@ -16,6 +20,11 @@ function getFloatFromTextInput(inputId) {
 function updateTextInputFromText(inputId, value) {
   const inputEl = document.getElementById(inputId);
   inputEl.value = value;
+}
+
+function checkLm317Vout(vRef, r1, r2, iAdj) {
+  // Verification: Vout = Vref (1 + R2 / R1) + Iadj R2
+  return vRef * (1 + r2 / r1) + iAdj * r2;
 }
 
 function update() {
@@ -35,15 +44,21 @@ function update() {
   const derivR2 = () => (inputVOut() - constVRef()) / (derivIRef() + inputIAdj());
 
   // Verification: Vout = Vref (1 + R2 / R1) + Iadj R2
-  const checkVOut = () => constVRef() * (1 + derivR2() / inputR1()) + inputIAdj() * derivR2();
+  const checkVOut = () => checkLm317Vout(constVRef(), inputR1(), derivR2(), inputIAdj());
 
   // Vin Recommended :: V
   const derivVInRecommended = () => inputVOut() + 3;
+
+  const derivR2E12 = () => nearestE12ValueFromValue(derivR2());
+  const checkVOutE12 = () => checkLm317Vout(constVRef(), inputR1(), derivR2E12(), inputIAdj());
 
   updateTextInputFromText('calc-r2__deriv-iref', derivIRef().toExponential(2));
   updateTextInputFromText('calc-r2__deriv-r2', derivR2().toFixed(0));
   updateTextInputFromText('calc-r2__check-vout', checkVOut().toFixed(2));
   updateTextInputFromText('calc-r2__deriv-vin-recommended', derivVInRecommended().toFixed(2));
+
+  updateTextInputFromText('calc-r2__deriv-r2-e12', derivR2E12().toFixed(0));
+  updateTextInputFromText('calc-r2__check-vout-e12', checkVOutE12().toFixed(2));
 }
 
 addEventListenerToId('calc-r2__input-vout', 'keyup', update);
